a6d43e437b7b966a97603d76b1865f87bceef72d
Khanittha Kraj <kkraj.swe@gmail.com>
2020-04-12 06:04:03 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,9 +22,7 @@ def bonus
 
   modified_hero = epic_tragedy[:montague][:hero][:status] = "dead"
   modified_heroine = epic_tragedy[:montague][:heroine][:status] = "dead"
-
-modified_hero
-modified_heroine 
+  
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
